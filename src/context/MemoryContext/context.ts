f601789256928661c08b9bebfa39db9e25bba83d
Antonio González <agonzalezg86@gmail.com>
2024-12-17 08:19:51 +0100

    fix: add type assertion for match number

--- src/context/MemoryContext/context.ts
+++ src/context/MemoryContext/context.ts
@@ -1,11 +1,12 @@
 import { createContext } from "react";
 import { MemoryContextProps, MemoryState } from "./types";
 import { generateShuffledCardsDeck } from "@/utils";
+import { MatchNumber } from "@/types";
 
 export const getMemoryContextInitialState = (): MemoryState => ({
 	cards: generateShuffledCardsDeck(),
 	attempts: 0,
-	matches: 0,
+	matches: 0 as MatchNumber,
 	isGameOver: false,
 });
 
